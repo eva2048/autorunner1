3366e8c8ae7e61f20d07868bb292372084f33fce
wuxingwei <614768242@qq.com>
2017-09-06 15:18:56 +0800

    9-6
    统计修改

--- app/js/controller/home/task/task.js
+++ app/js/controller/home/task/task.js
@@ -58,7 +58,7 @@ define(['app'], function(app) {
                             field: "action",
                             displayName: '操作处理',
                             minWidth:350,
-                            cellTemplate: '<div class="ui-grid-cell-contents tablecellfunc"><a class="f_blue cursor_p" ng-click="grid.appScope.open(\'runningDevice\',\'lg\')">立即执行</a><a class="f_blue">查看最新报表</a><a class="f_blue" ng-click="grid.appScope.open(\'createTaskTestset\')">修改测试集</a></div>',
+                            cellTemplate: '<div class="ui-grid-cell-contents tablecellfunc"><a class="f_blue cursor_p" ng-click="grid.appScope.open(\'runningDevice\',\'lg\')">立即执行</a><a class="f_blue" ui-sref="index.task.statementDetail">查看最新报表</a><a class="f_blue" ng-click="grid.appScope.open(\'createTaskTestset\')">修改测试集</a></div>',
                             enableColumnMenu: false, // 是否显示列头部菜单按钮
                             enableColumnResizing: false,
                         }
