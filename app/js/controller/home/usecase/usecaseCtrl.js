6fa5ab99d554ed3025f343498f7112421a47f6f8
wuxingwei <614768242@qq.com>
2017-11-06 16:40:05 +0800

    2017-11-6

--- app/js/controller/home/usecase/usecaseCtrl.js
+++ app/js/controller/home/usecase/usecaseCtrl.js
@@ -5,8 +5,7 @@ define(['app'], function(app) {
                 //表格初始化
                 $http.get('./data/usecase.php')
                     .success(function(data) {
-                        $scope.gridOptionsUsecase.data = data.lists;
-                        $scope.gridOptionsUsecaseData.data = data.lists;
+                        $scope.gridOptionsUsecase.data = data.lists;                      
                     });
                 //表格初始化
                 i18nService.setCurrentLang("zh-cn");
@@ -35,68 +34,55 @@ define(['app'], function(app) {
                             enableSorting: false,
                             enableColumnMenu: false, // 是否显示列头部菜单按钮
                             enableCellEdit: true, // 是否可编辑
-//                          cellTemplate: '<div class="f_blue ui-grid-cell-contents cursor_p" ng-click="grid.appScope.fadeIn();$event.stopPropagation();">{{row.entity.usecaseName}}</div>',
+                            cellTemplate: '<div class="f_blue ui-grid-cell-contents cursor_p">{{row.entity.usecaseName}}</div>',
                         },
-                        {
-                            field: "creater",
-                            displayName: '创建人',
-                            enableColumnMenu: false, // 是否显示列头部菜单按钮
-                        },
-                        {
-                            field: "createTime",
-                            displayName: '创建时间',
-                            enableColumnMenu: false, // 是否显示列头部菜单按钮
-                        },
+                                             
                         {
                             field: "connectUsecaseNum",
                             displayName: '关联缺陷数',
                             enableColumnMenu: false, // 是否显示列头部菜单按钮
-                        },
-                        {
-                            field: "connectDemandScene",
-                            displayName: '关联需求-场景',
+                            cellTemplate: '<div class="ui-grid-cell-contents tablecellfunc"><a class="f_blue" ui-sref="index.demand2">{{row.entity.connectUsecaseNum}}</a></div>',
+                        } ,
+                         {
+                            field: "creater",
+                            displayName: '创建人',
                             enableColumnMenu: false, // 是否显示列头部菜单按钮
-                        },
-//                      {
-//                          field: "action",
-//                          displayName: '操作处理',
-//                          cellTemplate: '<div class="ui-grid-cell-contents tablecellfunc"><a class="f_blue cursor_p" ng-click="grid.appScope.alertTip($event,\'aa\',\'aaaaaaaa\')">添加附件</a><a class="f_red">删除</a></div>',
-//                          enableColumnMenu: false, // 是否显示列头部菜单按钮
-//                      }
+                            enableColumnResizing: false,
+                        }, 
                     ],
                     data:[]
                 };
-                 //测试用例数据表
-                $scope.gridOptionsUsecaseData = {
-                    enableColumnResizing: true,
-                    rowHeight: 40,
-                    enableCellEdit: false,
-                    enableFiltering: true,
-                    selectionRowHeaderWidth: 40,
-                    paginationPageSizes: [10, 15, 20], //每页显示个数可选项
-                    enableHorizontalScrollbar: 0, //grid水平滚动条是否显示, 0-不显示  1-显示
-                    enableVerticalScrollbar: 0, //grid垂直滚动条是否显示, 0-不显示  1-显示
-                    columnDefs: [
-                        {
-                            field: "usecaseName",
-                            displayName: '数据名称',
-                            enableSorting: false,
-                            enableColumnMenu: false, // 是否显示列头部菜单按钮
-                            enableCellEdit: true // 是否可编辑
-                        },
-                        {
-                            field: "creater",
-                            displayName: '数据值',
-                            enableColumnMenu: false, // 是否显示列头部菜单按钮
-                            enableCellEdit: true // 是否可编辑
-                        },
-                        {
-                            field: "creater",
-                            displayName: '数据池',
-                            enableColumnMenu: false, // 是否显示列头部菜单按钮
-                        }
-                    ],
-                    data: []
-                };
+//               //测试用例数据表
+//              $scope.gridOptionsUsecaseData = {
+//                  enableColumnResizing: true,
+//                  rowHeight: 40,
+//                  enableCellEdit: false,
+//                  enableFiltering: true,
+//                  selectionRowHeaderWidth: 40,
+//                  paginationPageSizes: [10, 15, 20], //每页显示个数可选项
+//                  enableHorizontalScrollbar: 0, //grid水平滚动条是否显示, 0-不显示  1-显示
+//                  enableVerticalScrollbar: 0, //grid垂直滚动条是否显示, 0-不显示  1-显示
+//                  columnDefs: [
+//                      {
+//                          field: "usecaseName",
+//                          displayName: '数据名称',
+//                          enableSorting: false,
+//                          enableColumnMenu: false, // 是否显示列头部菜单按钮
+//                          enableCellEdit: true // 是否可编辑
+//                      },
+//                      {
+//                          field: "creater",
+//                          displayName: '数据值',
+//                          enableColumnMenu: false, // 是否显示列头部菜单按钮
+//                          enableCellEdit: true // 是否可编辑
+//                      },
+//                      {
+//                          field: "creater",
+//                          displayName: '数据池',
+//                          enableColumnMenu: false, // 是否显示列头部菜单按钮
+//                      }
+//                  ],
+//                  data: []
+//              };
             });
 })
